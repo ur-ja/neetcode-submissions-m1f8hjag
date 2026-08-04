99a484d78169c060b97259f0d1b96536c60eff56
ur-ja <67295135+ur-ja@users.noreply.github.com>
2026-08-04 19:03:34 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 1:
+            return 1
+        dp = [-1] * n
+        dp[0] = 1
+        dp[1] = 2
+        for i in range(2, n):
+            left = dp[i - 1]
+            right = dp[i - 2]
+            dp[i] = left + right
+
+        return dp[n - 1]
